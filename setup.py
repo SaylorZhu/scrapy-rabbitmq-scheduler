41661ee9cad20de901e618016ec40724c4ff7c35
SaylorZhu <531301071@qq.com>
2025-04-07 16:48:34 +0800

    1.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     # 以下为必需参数
     name='scrapy-rabbitmq-scheduler-saylor',  # 模块名
-    version='1.0.2',  # 当前版本
+    version='1.0.3',  # 当前版本
     description='Rabbitmq for Distributed scraping',  # 简短描述
     author='saylorzhu',
     author_email='531301071@qq.com',
